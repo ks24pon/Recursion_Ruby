375a818a7d7e2662f52ce7ff01bf117c4edd10f6
ks24pon <pom123re0624@gmail.com>
2025-01-30 21:25:15 +0900

    変数と代入演算子

---
+++ assignment/sample5.rb
@@ -0,0 +1,15 @@
+myPower = 300
+mySpeed = 200
+myTechnique = 50
+myDefense = 500
+
+# ここから書きましょう
+myPower += 10
+mySpeed -= 5
+myTechnique *= 7
+myDefense /= 2
+
+puts "myPower : #{myPower}"
+puts "mySpeed : #{mySpeed}"
+puts "myTechnique : #{myTechnique}"
+puts "myDefense : #{myDefense}"
