4bd9ca0d4fad2f7c79dfa620a544b30aa890acf4
ks24pon <pom123re0624@gmail.com>
2025-02-01 12:59:25 +0900

    名前のイニシャル

---
+++ assignment/sampl14.rb
@@ -0,0 +1,7 @@
+def nameInitials(firstName,lastName)
+    return firstName[0] + "." + lastName[0]
+end
+
+puts(nameInitials("Steve","Jobs"))
+puts(nameInitials("Donald","Trump"))
+puts(nameInitials("Taro","Yamada"))
