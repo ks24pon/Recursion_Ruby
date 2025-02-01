2f066e9eef0f36ba33ae35e22cd13062be7fd8e3
ks24pon <pom123re0624@gmail.com>
2025-02-01 11:49:21 +0900

    引き算

---
+++ assignment/sample6.rb
@@ -0,0 +1,10 @@
+def getLowestTemperature(x,y)
+    return x - y
+end
+
+## 出力
+puts(getLowestTemperature(3,2))
+puts(getLowestTemperature(2,10))
+puts(getLowestTemperature(18,5))
+puts(getLowestTemperature(8,14))
+puts(getLowestTemperature(20,5))
