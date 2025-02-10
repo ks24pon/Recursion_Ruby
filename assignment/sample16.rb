93ba6666d56b594aa5d5d18845c317a92278fd61
ks24pon <pom123re0624@gmail.com>
2025-02-10 22:31:41 +0900

    ts

---
+++ assignment/sample16.rb
@@ -0,0 +1,13 @@
+def isOdd(n)
+    return n % 2 == 1
+end
+
+puts isOdd(1)
+puts isOdd(2)
+puts isOdd(3)
+puts isOdd(4)
+puts isOdd(5)
+puts isOdd(6)
+puts isOdd(7)
+puts isOdd(8)
+puts isOdd(9)
---
+++ assignment/sample17.rb
@@ -0,0 +1,9 @@
+def isLastNameLonger(firstName,lastName)
+    return firstName.length <= lastName.length
+end
+
+
+puts isLastNameLonger("Enrico","Bernice")
+puts isLastNameLonger("Treutel","Harvey")
+puts isLastNameLonger("Brakus","Krajcik")
+puts isLastNameLonger("Steven","Donald")
