0bb2bc4efc936aaae9fdb8de1e31800374887331
ks24pon <pom123re0624@gmail.com>
2025-02-13 22:36:18 +0900

    if文

---
+++ sample25.rb
@@ -0,0 +1,12 @@
+def myXOR(p,q)
+    if p != q
+        true
+    else
+        false
+    end
+end
+
+puts myXOR(true, false)
+puts myXOR(false, false)
+puts myXOR(false, false)
+puts myXOR(true, true)
