261b9c04f7d8f2eed03c74d427f53ad4ebc86742
ks24pon <pom123re0624@gmail.com>
2025-04-16 13:21:19 +0900

    インデント直し

--- assignment/sample94.rb
+++ assignment/sample94.rb
@@ -1,25 +1,25 @@
 # Product Class作成
 class Product
-    # getter/setter自動生成
-    attr_accessor :title, :price
-    # 初期化(newされた時に呼ばれる)
-    def initialize(title, price)
-        # インスタンス変数@titleに引数title(商品名)を代入
-        @title = title
-        # @priceに引数price(価格)を代入
-        @price = price
-    end
+  # getter/setter自動生成
+  attr_accessor :title, :price
+
+  # 初期化(newされた時に呼ばれる)
+  def initialize(title, price)
+    # インスタンス変数@titleに引数title(商品名)を代入
+    @title = title
+    # @priceに引数price(価格)を代入
+    @price = price
+  end
 end
 
 # テストケース
 
 # インスタンス作成
 product1 = Product.new("shampoo", 10)
-#productオブジェクトからtitleを取得
+# productオブジェクトからtitleを取得
 puts product1.title
 puts product1.price
 
 product2 = Product.new("conditioner", 5)
 puts product2.title
 puts product2.price
-
