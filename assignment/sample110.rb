aa4b6215bd6adb4d08f9a3982f61b8f2373f0945
ks24pon <pom123re0624@gmail.com>
2025-04-18 09:28:28 +0900

    文字コード

---
+++ assignment/sample110.rb
@@ -0,0 +1,9 @@
+def isFirstStringLarger(s1,s2)
+  # s1を小文字に変換して、1文字ずつ配列に分解し、各文字をASCIIコードに変換し、合計を計算
+  s1_sum = s1.downcase.chars.map(&:ord).sum
+  # s2もs1と同様
+  s2_sum = s2.downcase.chars.map(&:ord).sum
+  # s1の合計がs2より大きいか判定(true/false)
+  s1_sum > s2_sum
+end
+
