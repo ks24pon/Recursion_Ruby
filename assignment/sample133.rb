d2a67facf4006ca113cb30b60b715322aaa3156d
ks24pon <pom123re0624@gmail.com>
2025-04-22 11:45:38 +0900

    連想配列

---
+++ assignment/sample133.rb
@@ -0,0 +1,12 @@
+price_table = {
+    "Honda Civic" => "24000",
+    "Chevrolet Traverse" => "30000",
+    "Toyota Camry" => "25000",
+    "Subaru Outback" => "27000",
+    "Tesla X" => "100000"
+}
+
+# テストケース
+puts price_table["Tesla X"]
+puts price_table["Toyota Camry"]
+puts price_table["Chevrolet Traverse"]
