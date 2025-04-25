f0fa346a724274f3a87dd54b8d74817eb1c9cb26
ks24pon <pom123re0624@gmail.com>
2025-04-25 10:12:44 +0900

    リスト検索

---
+++ assignment/sample143.rb
@@ -0,0 +1,14 @@
+def searchList(num_list,value)
+  # ハッシュマップ初期化
+  hash_map = {}
+
+  num_list.each_with_index do |num, index|
+      # まだ登録されていない場合だけ登録(最初のインデックス優先)
+      hash_map[num] ||= index
+  end
+
+  # 探しているハッシュに含まれていればインデックスを返す
+  # なければ-1を返す
+  hash_map.key?(value) ? hash_map[value] : -1
+end
+
